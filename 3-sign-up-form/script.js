3ce7125d5f0dc274958bb92d63b1f4a864d1a652
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-04-29 06:52:55 +0300

    add email validation using HTML, CSS and JavaScript

--- 3-sign-up-form/script.js
+++ 3-sign-up-form/script.js
@@ -1,26 +1,32 @@
+// Email Validation
+const emailInput = document.querySelector('[type="email"]');
+const emailError = document.querySelector('.error--email');
+emailInput.addEventListener('input', (e) => {
+  if (!e.target.value.includes('@')) {
+    emailError.style.visibility = 'visible';
+  } else {
+    emailError.style.visibility = 'hidden';
+  }
+});
+
+// Password Validation
 const passwordInput = document.querySelector('[name="password"]');
 const passwordError = document.querySelector('.error--password');
-
-const passwordConfirm = document.querySelector('[name="confirm-password"]');
-const passwordConfirmError = document.querySelector('.error--confirm');
-
-const form = document.querySelector('.form');
-
-
-
 passwordInput.addEventListener('input', (e) => {
-  if(e.target.value.length <= 4) {
+  if (e.target.value.length <= 4) {
     passwordError.style.visibility = 'visible';
   } else {
-    passwordError.style.visibility  = 'hidden';
+    passwordError.style.visibility = 'hidden';
   }
-})
+});
 
+// Confirm Password Validation
+const passwordConfirm = document.querySelector('[name="confirm-password"]');
+const passwordConfirmError = document.querySelector('.error--confirm');
 passwordConfirm.addEventListener('input', (e) => {
-
-  if(e.target.value !== passwordInput.value) {
+  if (e.target.value !== passwordInput.value) {
     passwordConfirmError.style.visibility = 'visible';
   } else {
     passwordConfirmError.style.visibility = 'hidden';
   }
-})
+});
