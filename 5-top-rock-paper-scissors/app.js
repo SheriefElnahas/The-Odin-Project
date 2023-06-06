64e8ff287dd089612652fb9b342352db840f9a41
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-06-06 07:38:43 +0300

    fix HTML & CSS

--- 5-top-rock-paper-scissors/app.js
+++ 5-top-rock-paper-scissors/app.js
@@ -1,19 +1,19 @@
-/********************************************************************************************************/  
-/*------------------- DOM Selection -----------------*/ 
+/********************************************************************************************************/
+/*------------------- DOM Selection -----------------*/
 // Modal & Overlay
 const modal = document.querySelector('.modal');
-const modalText = document.querySelector('.modal-text');
-const overlay = document.querySelector('.overlay');
+const modalText = document.querySelector('.modal__text');
+// const overlay = document.querySelector('.overlay');
 
 
 // Show Result Element & Show Details Element
-const resultElement = document.querySelector('.result');
-const detailsElement = document.querySelector('.details');
+const resultElement = document.querySelector('.game__result');
+const detailsElement = document.querySelector('.game__details');
 
 
 // Player & Computer Choice ( Rock , Paper Or Scissors )) 
-const playerChoiceElement = document.querySelector('.player-choice');
-const computerChoiceElement = document.querySelector('.computer-choice');
+const playerChoiceElement = document.querySelector('.player__choice');
+const computerChoiceElement = document.querySelector('.computer__choice');
 
 // Player & Computer Score Html Element
 const playerScoreElement = document.querySelector('.player-score');
@@ -25,34 +25,34 @@ const buttons = document.querySelectorAll('.btn');
 
 
 // Reset Button & Game Over Variable To Check Game State
-const playAgain = document.querySelector('.modal-button');
+const playAgain = document.querySelector('.modal__button');
 let gameOver = false;
 
 
 // Track Player & Computer Score To Determine The Winner
 let playerScore = 0;
 let computerScore = 0;
-/********************************************************************************************************/ 
+/********************************************************************************************************/
 
 
 
 
-/********************************************************************************************************/  
-/*------------------- Logic -----------------*/ 
+/********************************************************************************************************/
+/*------------------- Logic -----------------*/
 const choices = ['rock', 'paper', 'scissors'];
 
 // Helper Function
 const randomNum = () => Math.floor(Math.random() * 3);
 
 function computerPlay() {
-    return  choices[randomNum()];
+    return choices[randomNum()];
 }
 
 // Function To Change From String To Emoji
 function changeToEmoji(choice) {
-    if(choice === 'paper') {
+    if (choice === 'paper') {
         choice = '✋';
-    } else if(choice === 'rock') {
+    } else if (choice === 'rock') {
         choice = '✊';
     } else if (choice === 'scissors') {
         choice = '✌';
@@ -61,22 +61,22 @@ function changeToEmoji(choice) {
 }
 
 
-function checkWinner(playerSelection,computerSelection) {
-    if( playerSelection === 'rock' && computerSelection === 'scissors'  ||
-        playerSelection === 'scissors' && computerSelection === 'paper'||
-        playerSelection === 'paper' && computerSelection === 'rock'     ) {
+function checkWinner(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+        playerSelection === 'scissors' && computerSelection === 'paper' ||
+        playerSelection === 'paper' && computerSelection === 'rock') {
 
-         // Player Wins
-         playerScore++;
-        return `You Won! ${playerSelection} beats ${computerSelection}`   
-       } else if 
-     ( computerSelection === 'rock' && playerSelection === 'scissors'    ||
-       computerSelection === 'scissors' && playerSelection === 'paper' ||
-       computerSelection === 'paper' && playerSelection === 'rock' ) {
+        // Player Wins
+        playerScore++;
+        return `You Won! ${playerSelection} beats ${computerSelection}`
+    } else if
+        (computerSelection === 'rock' && playerSelection === 'scissors' ||
+        computerSelection === 'scissors' && playerSelection === 'paper' ||
+        computerSelection === 'paper' && playerSelection === 'rock') {
 
         // Computer Wins
         computerScore++;
-        return `You Lost! ${playerSelection} beats ${computerSelection}`   
+        return `You Lost! ${playerSelection} beats ${computerSelection}`
     } else {
 
         // Draw
@@ -93,37 +93,37 @@ buttons.forEach((button) => {
         const computerChoice = computerPlay();
 
         // Get The Result Of Playing
-        const result = checkWinner(playerChoice , computerChoice);
+        const result = checkWinner(playerChoice, computerChoice);
 
 
         // Stop The Game Of The Score is 5 & Hide The Modal & Overlay
-        if(playerScore === 5 || computerScore === 5) {
+        if (playerScore === 5 || computerScore === 5) {
             gameOver = true;
             modalText.textContent = result.split('!')[0] + "!";
-            modal.classList.remove('hide');
-            overlay.classList.remove('hide');
+            modal.showModal();
+
         }
 
-        if(!gameOver) {
+        if (!gameOver) {
             // Display Player & Computer Choice
             playerChoiceElement.textContent = changeToEmoji(playerChoice);
             computerChoiceElement.textContent = changeToEmoji(computerChoice);
-    
+
             // Display The Result & Details 
             resultElement.textContent = result.split('!')[0] + "!";
-            detailsElement.textContent = result.split('!')[1] ;
+            detailsElement.textContent = result.split('!')[1];
         }
 
         // Display Player & Computer Choice
-         playerScoreElement.textContent = playerScore;
-         computerScoreElement.textContent = computerScore;
+        playerScoreElement.textContent = playerScore;
+        computerScoreElement.textContent = computerScore;
     })
 })
 
 function reset() {
     // Remove The Modal & Overlay
-    modal.classList.add('hide');
-    overlay.classList.add('hide');
+
+    modal.close();
 
     // Reset Result & Details Text 
     resultElement.textContent = 'Choose your weapon!';
@@ -141,12 +141,12 @@ function reset() {
     gameOver = false;
 }
 
-playAgain.addEventListener('click', function() {
+playAgain.addEventListener('click', function () {
     reset();
 });
 
 
-/********************************************************************************************************/  
+/********************************************************************************************************/
 
 
 
