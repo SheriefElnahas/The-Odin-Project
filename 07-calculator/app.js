d1a632d9f3b5cf11f3f7e78ac49fac04811963d0
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-06-08 03:27:06 +0300

    handle . point cases

--- 07-calculator/app.js
+++ 07-calculator/app.js
@@ -1,19 +1,30 @@
 const screenLastElement = document.querySelector('.screen__last');
 const screenResultElement = document.querySelector('.screen__result');
 
-
 const calculatorButtons = document.querySelector('.calculator__buttons');
-
 
 calculatorButtons.addEventListener('click', (e) => {
   if (e.target.tagName === 'BUTTON') {
-    const currentValue = screenResultElement.textContent;
-    const buttonValue = e.target.textContent;
+    if (!isNaN(parseInt(e.target.textContent)) || e.target.textContent === '.') {
+      const currentValue = screenResultElement.textContent;
+      const buttonValue = e.target.textContent;
 
-    if (currentValue === '0') {
-      screenResultElement.textContent = buttonValue;
-    } else {
-      screenResultElement.textContent += buttonValue;
+      // prevent the user from enterting more than one .
+      if (screenResultElement.textContent.includes('.') && e.target.textContent === '.') {
+        return;
+      }
+
+      if (currentValue === '0') {
+        // handle the case to allow the user to enter floating numbers!
+        if (e.target.textContent === '.') {
+          screenResultElement.textContent += buttonValue;
+          return;
+        }
+
+        screenResultElement.textContent = buttonValue;
+      } else {
+        screenResultElement.textContent += buttonValue;
+      }
     }
   }
 });
