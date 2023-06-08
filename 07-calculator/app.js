fbd74349dd636dd20ac5a8127b170f7158c67044
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-06-08 04:50:50 +0300

    extract logic into smaller functions

--- 07-calculator/app.js
+++ 07-calculator/app.js
@@ -3,28 +3,46 @@ const screenResultElement = document.querySelector('.screen__result');
 
 const calculatorButtons = document.querySelector('.calculator__buttons');
 
-calculatorButtons.addEventListener('click', (e) => {
+calculatorButtons.addEventListener('click', handleButtonClick);
+
+
+function isNumberOrDecimalPoint(value) {
+  return !isNaN(value) || value === '.';
+}
+
+function handleButtonClick(e) {
   if (e.target.tagName === 'BUTTON') {
-    if (!isNaN(parseInt(e.target.textContent)) || e.target.textContent === '.') {
-      const currentValue = screenResultElement.textContent;
-      const buttonValue = e.target.textContent;
+    const buttonValue = e.target.textContent;
 
-      // prevent the user from enterting more than one .
-      if (screenResultElement.textContent.includes('.') && e.target.textContent === '.') {
-        return;
-      }
-
-      if (currentValue === '0') {
-        // handle the case to allow the user to enter floating numbers!
-        if (e.target.textContent === '.') {
-          screenResultElement.textContent += buttonValue;
-          return;
-        }
-
-        screenResultElement.textContent = buttonValue;
-      } else {
-        screenResultElement.textContent += buttonValue;
-      }
+    // If buttonValue is a number or decimal point then call handleButtonClick function
+    if (isNumberOrDecimalPoint(buttonValue)) {
+      handleNumberClick(buttonValue);
     }
+
+
+    // If buttonvalue is not a number then call handleopeartion
   }
-});
+}
+
+function handleNumberClick(buttonValue) {
+  const currentValue = screenResultElement.textContent;
+
+  // prevent the user from enterting more than one .
+  if (screenResultElement.textContent.includes('.') && buttonValue === '.') {
+    return;
+  }
+
+  if (currentValue === '0') {
+    // handle the case to allow the user to enter floating numbers!
+    if (buttonValue === '.') {
+      screenResultElement.textContent += buttonValue;
+      return;
+    }
+    screenResultElement.textContent = buttonValue;
+  } else {
+    screenResultElement.textContent += buttonValue;
+  }
+}
+
+
+
