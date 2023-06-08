c58a9512fe28fdd391e2343401c2f74c0ab69d75
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-06-08 03:10:49 +0300

    handle enterting numbers and floating numbers

--- 07-calculator/app.js
+++ 07-calculator/app.js
@@ -1,9 +1,19 @@
-const resultScreenElement = document.querySelector('.calculator__result');
+const screenLastElement = document.querySelector('.screen__last');
+const screenResultElement = document.querySelector('.screen__result');
+
+
 const calculatorButtons = document.querySelector('.calculator__buttons');
 
-calculatorButtons.addEventListener('click', (e) => {
 
-  if(e.target.tagName === 'BUTTON') {
-    console.log(e.target.textContent);
+calculatorButtons.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const currentValue = screenResultElement.textContent;
+    const buttonValue = e.target.textContent;
+
+    if (currentValue === '0') {
+      screenResultElement.textContent = buttonValue;
+    } else {
+      screenResultElement.textContent += buttonValue;
+    }
   }
-})
+});
